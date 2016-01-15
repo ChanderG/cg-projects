1e9c58c0ec7008b3636effad5000055b0173dc06
ChanderG <chandergovind@gmail.com>
2016-01-15 23:28:07 +0530

    add faces code; NOT WORKING
    some problems occur

--- main.c
+++ main.c
@@ -82,45 +82,81 @@ Point *draw3DLine(Point s, Point e, size_t *len){
 char* returnVoxelCube(Point center, int* offset){
 	char* repn = malloc(500*sizeof(char)); //TODO: use a more appropriate number here.
 	// get representation here
-	sprintf(repn, "#Hello"); // TODO: put correct stuff here
-	strcat(repn, " Bye \n");
 	
 	// calculate vertices and faces here
 	int vertex_line_length = 25;
-	char* vertex_line_repn = "v %f %f %f %f\n";
+	char* vertex_line_repn = "v %f %f %f \n";
+
 	char *v1 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v1, vertex_line_repn, center.x - 0.5, center.y - 0.5, center.z + 0.5);
-	strcat(repn, v1);
 
 	char *v2 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v2, vertex_line_repn, center.x + 0.5, center.y - 0.5, center.z + 0.5);
-	strcat(repn, v2);
 
 	char *v3 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v3, vertex_line_repn, center.x + 0.5, center.y + 0.5, center.z + 0.5);
-	strcat(repn, v3);
 
 	char *v4 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v4, vertex_line_repn, center.x - 0.5, center.y + 0.5, center.z + 0.5);
-	strcat(repn, v4);
 
 	char *v7 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v7, vertex_line_repn, center.x - 0.5, center.y - 0.5, center.z - 0.5);
-	strcat(repn, v7);
 
 	char *v6 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v6, vertex_line_repn, center.x + 0.5, center.y - 0.5, center.z - 0.5);
-	strcat(repn, v6);
 
 	char *v5 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v5, vertex_line_repn, center.x + 0.5, center.y + 0.5, center.z - 0.5);
-	strcat(repn, v5);
 
 	char *v8 = malloc(vertex_line_length*sizeof(char));
 	sprintf(v8, vertex_line_repn, center.x - 0.5, center.y + 0.5, center.z - 0.5);
-	strcat(repn, v8);
 	
+	strcat(repn, v1);
+	strcat(repn, v2);
+	strcat(repn, v3);
+	strcat(repn, v4);
+	strcat(repn, v5);
+	strcat(repn, v6);
+	strcat(repn, v7);
+	strcat(repn, v8);
+
+	// dealing with faces
+	int face_line_length = 20;
+	char *f1, *f2, *f3, *f4, *f5, *f6; 
+	f1 = malloc(face_line_length*sizeof(char));
+	f2 = malloc(face_line_length*sizeof(char));
+	f3 = malloc(face_line_length*sizeof(char));
+	f4 = malloc(face_line_length*sizeof(char));
+	f5 = malloc(face_line_length*sizeof(char));
+	f6 = malloc(face_line_length*sizeof(char));
+
+    // TODO: use #defines to make all this less verbose
+	int _1 = 1 + *offset;
+	int _2 = 2 + *offset;
+	int _3 = 3 + *offset;
+	int _4 = 4 + *offset;
+	int _5 = 5 + *offset;
+	int _6 = 6 + *offset;
+	int _7 = 7 + *offset;
+	int _8 = 8 + *offset;
+	
+	char *face_line_repn = "f %d %d %d %d\n";
+	sprintf(f1, face_line_repn, _1, _2, _3, _4);
+	sprintf(f2, face_line_repn, _5, _6, _7, _8);
+	sprintf(f3, face_line_repn, _2, _6, _5, _3);
+	sprintf(f4, face_line_repn, _8, _7, _1, _4);
+	sprintf(f5, face_line_repn, _7, _6, _2, _1);
+	sprintf(f6, face_line_repn, _3, _5, _8, _4);
+
+	strcat(repn, f1);
+	strcat(repn, f2);
+	strcat(repn, f3);
+	strcat(repn, f4);
+	strcat(repn, f5);
+	strcat(repn, f6);
+
 	// TODO: use and update offset
+	*offset += 14;
     return repn;
 }
 
@@ -138,9 +174,12 @@ void createVoxelImage(Point* pts, size_t len){
 	int offset = 0;
 	for (size_t i = 0; i < len; i++){
 		//output each cube's info
-		char* cube_string = returnVoxelCube(pts[i], &offset);
+        Point o = (Point){ .x = 0, .y = 0, .z = 0};
+		char* cube_string = returnVoxelCube(o, &offset);
+		//char* cube_string = returnVoxelCube(pts[i], &offset);
 		fputs(cube_string, fp);
-		fputs("\n", fp);
+		fputs("#\n", fp);
+		break;  //TODO: REMOVE THIS
 	}
 	fclose(fp);
 }
