8f38e8c12dfcb8b059dd190dc4eae4ac32c35e79
ChanderG <chandergovind@gmail.com>
2016-02-01 11:59:52 +0530

    add computation to obtain plane of triangle

--- triangle/triangle3d.c
+++ triangle/triangle3d.c
@@ -12,7 +12,21 @@
  *          populates len variable with length of said array
  */
 Point* getTriangle3dPoints(Point a, Point b, Point c, size_t* len){
+
+  logm("getTriangle3dPoints", "a (%d, %d, %d) b (%d, %d, %d) c (%d, %d, %d)", a.x, a.y, a.z, b.x, b.y, b.z, c.x, c.y, c.z);
   
+  // first calculate coefficients of plane
+  int A, B, C, D;
+
+  A = ((b.y - a.y)*(c.z - a.z) - (b.z - a.z)*(c.y - a.y));
+  B = ((b.z - a.z)*(c.x - a.x) - (c.z - a.z)*(b.x - a.x));
+  C = ((b.x - a.x)*(c.y - a.y) - (b.y - a.y)*(c.x - a.x));
+
+  // substituting a into equation to obtain D as well
+  D = 0 - A*a.x - B*a.y - C*a.z;
+
+  logm("getTriangle3dPoints", "Plane params: A = %d, B = %d, C = %d, D = %d", A, B, C, D);
+
   // for now, just add endpoints
   Point* pts = malloc(3*sizeof(Point));
   pts[0] = a;
